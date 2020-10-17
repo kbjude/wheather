e992c552c29d8717a0e54e72c1b8a9cf6a237547
Jude Kajura <kbjude@gmail.com>
2020-10-17 19:21:48 +0300

    Adding logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import _ from 'lodash';
 
+const result = document.createElement('div');
+const button = document.createElement('button');
+button.setAttribute('content', 'Get API content')
+
 function component() {
   const element = document.createElement('div');
 
@@ -9,3 +13,17 @@ function component() {
 }
 
 document.body.appendChild(component());
+document.body.appendChild(result);
+const getButton = document.getElementById('extract-weather');
+
+
+async function getMap() {
+  try {
+    const wmap = await fetch('https://api.giphy.com/v1/gifs/translate?api_key=2nbl8h7s2hbDV8ZimbLX9Y9nAO1TCGwo&s=cats', { mode: 'cors' });
+    const wdata = await wmap.json();
+    console.log (wdata.data.images.origina.url);
+  } catch (err) {
+    return err;
+  }
+}
+getButton.addEventListener('click', getMap);
