c842f4625f5b4493744d7e463591a62d8501c2c8
Jude Kajura <kbjude@gmail.com>
2020-10-20 01:44:18 +0300

    adding an immage

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const name = document.querySelector('.name');
 const description = document.querySelector('.desc');
 const temperature = document.querySelector('.temp');
 const humidity = document.querySelector('.humidity');
+const img = document.querySelector('.icon');
 
 // document.body.appendChild(result);
 const getButton = document.getElementById('extract-weather');
@@ -13,17 +14,19 @@ const getButton = document.getElementById('extract-weather');
 const show = document.querySelector('display');
 async function getMap() {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&APPID=`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&APPID=d525220abdcad907f9e83654cb6f042d`, { mode: 'cors' });
     const json = await response.json();
     const nameValue = json.name;
     const tempValue = json.main.temp;
     const descdata = json.weather[0].description;
     const humid = json.main.humidity;
+    const newImg = json.whether.icon;
 
     name.innerHTML = nameValue;
     temperature.innerHTML = tempValue;
     description.innerHTML = descdata;
-    humidity.innerHTML = humid
+    humidity.innerHTML = humid;
+    img. = newImg;
   } catch (err) {
     return err;
   }
