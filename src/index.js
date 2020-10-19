42b9af84c1f2cbadd432a605e54754ff4d10b582
Jude Kajura <kbjude@gmail.com>
2020-10-19 21:13:05 +0300

    key hide

--- src/index.js
+++ src/index.js
@@ -7,30 +7,23 @@ const description = document.querySelector('.desc');
 const temperature = document.querySelector('.temp');
 const humidity = document.querySelector('.humidity');
 
-function component() {
-  const element = document.createElement('div');
-
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
-}
-
-document.body.appendChild(component());
 // document.body.appendChild(result);
 const getButton = document.getElementById('extract-weather');
 
 const show = document.querySelector('display');
 async function getMap() {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&APPID=d525220abdcad907f9e83654cb6f042d`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&APPID=`, { mode: 'cors' });
     const json = await response.json();
     const nameValue = json.name;
     const tempValue = json.main.temp;
     const descdata = json.weather[0].description;
+    const humid = json.main.humidity;
 
     name.innerHTML = nameValue;
     temperature.innerHTML = tempValue;
     description.innerHTML = descdata;
+    humidity.innerHTML = humid
   } catch (err) {
     return err;
   }
