75b49056b40f6237a310c04cdd68f23ae8a2236b
Jude Kajura <kbjude@gmail.com>
2020-10-20 18:43:13 +0300

    Validating the field

---
+++ src/validation.js
@@ -0,0 +1,9 @@
+
+
+function validateForm() {
+    const getField = document.getElementById('input');
+    if (getField == ""){
+        alert("This field can't be empty");
+        return false
+    }
+}
