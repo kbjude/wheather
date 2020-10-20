d23224d59ea7a8f9c7960cc2d9481b34202d68ea
Jude Kajura <kbjude@gmail.com>
2020-10-20 20:25:26 +0300

    WIP validations

--- src/validation.js
+++ src/validation.js
@@ -1,11 +1,11 @@
 import getMap from './index';
 
-const validateForm = (() => {
-  const getField = document.getElementById('input');
+function validateForm () {
+  const getField = document.getElementById('input').value;
   if (getField === '') {
     alert("This field can't be empty");
     return false;
   }
-});
+}
 
 export default validateForm;
