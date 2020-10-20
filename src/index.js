dca2137ef0b158600f40218953c614d552978901
Jude Kajura <kbjude@gmail.com>
2020-10-21 00:25:10 +0300

    Adding the icons and the WIP toggle

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ const name = document.querySelector('.name');
 const description = document.querySelector('.desc');
 const temperature = document.querySelector('.temp');
 const humidity = document.querySelector('.humidity');
+const toggle = document.querySelector('toggle');
 // const wheatherIcon = document.querySelector('.icon');
-// const img = document.querySelector('.icon');
+const img = document.querySelector('.icon1');
 
 // document.body.appendChild(result);
 const getButton = document.getElementById('extract-weather');
@@ -16,23 +17,34 @@ const getButton = document.getElementById('extract-weather');
 const show = document.querySelector('display');
 async function getMap() {
   try {
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&APPID=d525220abdcad907f9e83654cb6f042d`, { mode: 'cors' });
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&units=metric&APPID=d525220abdcad907f9e83654cb6f042d`, { mode: 'cors' });
     const json = await response.json();
     const nameValue = json.name;
     const tempValue = json.main.temp;
     const descdata = json.weather[0].description;
     const humid = json.main.humidity;
-    // const newImg = json.weather[0].icon;
-
+  
     name.innerHTML = nameValue;
     temperature.innerHTML = tempValue;
     description.innerHTML = descdata;
     humidity.innerHTML = humid;
+    img.src = `https://fernando-bc.com/weather-icons/${json.weather[0].icon}.svg`;
     // wheatherIcon.innerHTML = newImg;
+    const celsiusToFahrenheit = (celsius) => {
+      const newTemp = (celsius * (9 / 5)) + 32;
+      return newTemp;
+    };
+    console.log(celsiusToFahrenheit(tempValue));
   } catch (err) {
     return err;
   }
 }
 getButton.addEventListener('click', getMap, validateForm);
+
+
+const FahrenheitTocelsius = (Fahrenheit) => {
+  const newTemp = (fahrenheit - 32) * (5 / 9);
+  return newTemp;
+};
 
 export default getMap;
