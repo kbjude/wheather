b68d375b12d1f1749ae00921ccaeeb40fbe3bd98
Jude Kajura <kbjude@gmail.com>
2020-10-22 09:02:27 +0300

    Adding changes from package.json and ctoF

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const name = document.querySelector('.name');
 const description = document.querySelector('.desc');
 const temperature = document.querySelector('.temp');
 const humidity = document.querySelector('.humidity');
-const toggle = document.querySelector('.toggle');
+const btntoggle = document.querySelector('.toggle');
 // const wheatherIcon = document.querySelector('.icon');
 const img = document.querySelector('.icon1');
 
@@ -15,6 +15,11 @@ const img = document.querySelector('.icon1');
 const getButton = document.getElementById('extract-weather');
 
 const show = document.querySelector('display');
+
+const weatherValues = {
+
+};
+
 async function getMap() {
   try {
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchfield.value}&units=metric&APPID=d525220abdcad907f9e83654cb6f042d`, { mode: 'cors' });
@@ -23,37 +28,44 @@ async function getMap() {
     const tempValue = json.main.temp;
     const descdata = json.weather[0].description;
     const humid = json.main.humidity;
-  
+    weatherValues.nameValue = nameValue;
+    weatherValues.tempValue = tempValue;
+    weatherValues.descdata = descdata;
+    weatherValues.humid = humid;
+
     name.innerHTML = nameValue;
     temperature.innerHTML = tempValue;
     description.innerHTML = descdata;
     humidity.innerHTML = humid;
     img.src = `https://fernando-bc.com/weather-icons/${json.weather[0].icon}.svg`;
     // wheatherIcon.innerHTML = newImg;
-    const celsiusToFahrenheit = (celsius) => {
-      const newTemp = (celsius * (9 / 5)) + 32;
-      return newTemp;
-    };
-    console.log(celsiusToFahrenheit(tempValue));
   } catch (err) {
     return err;
   }
+  console.log(weatherValues);
+  // return weatherValues;
 }
 
-function toggle(){
-  if (weather.temperature.unit === underfined) return;
-  if (weather.temperature.unit === 'celsius'){
-    let fahrenheight = celciusToFahrenheight(weather.temperature.value);
-    fahrenheight = Math.floor(fahreinheight);
-    toggle.innerHTML = `${fahrenheight}o <span>F</span>`;
-    weather.temperature.unit = "fahrenheight";
-  } else {
-    toggle.innerHTML = `${weather.temperature.value}o <span>C</span>`;
-    weather.temperature.unit = "celsius";
-  }
-}
+const celsiusToFahrenheit = async (celsius) => {
+  const newTemp = (celsius * (9 / 5)) + 32;
+  return newTemp;
+};
+console.log(celsiusToFahrenheit(weatherValues.tempValue));
+
+// function toggle() {
+//   const temperatureUnit = 'celsius';
+//   if (temperatureUnit === undefined) return;
+//   if (temperatureUnit === 'celsius') {
+//     let fahrenheight = celsiusToFahrenheit(weather.temperature.value);
+//     fahrenheight = Math.floor(fahreinheight);
+//     btntoggle.innerHTML = `${fahrenheight}o <span>F</span>`;
+//     weather.temperature.unit = 'fahrenheight';
+//   } else {
+//     btntoggle.innerHTML = `${weather.temperature.value}o <span>C</span>`;
+//     weather.temperature.unit = 'celsius';
+//   }
+// }
 
 getButton.addEventListener('click', getMap);
-}
 
 export default getMap;
